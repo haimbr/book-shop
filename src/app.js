686303ec8f380a4165683d8fe9e216e68a21aacd
haimbr <70588165+haimbr@users.noreply.github.com>
2021-02-16 20:42:36 +0200

    test-2

--- src/app.js
+++ src/app.js
@@ -36,10 +36,10 @@ app.get('*', checkUser);
 
 app.get('/', async (req, res) =>{
     try{
-        const books = await Book.find({}).limit(20);
-        books.forEach(book => book.img = `data:image/png;base64,${book.img.toString('base64')}`);
-        res.render('index', {books})
-        // res.render('index')
+        // const books = await Book.find({}).limit(20);
+        // books.forEach(book => book.img = `data:image/png;base64,${book.img.toString('base64')}`);
+        // res.render('index', {books})
+        res.render('index')
     }catch(e){  
         console.log(e) 
         res.status(400).send(e);
